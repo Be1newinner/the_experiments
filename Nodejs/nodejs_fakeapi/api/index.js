f193956f08c80484ac26b9b322bfef4b8670e633
Vijay Kumar <Be1newinner@gmail.com>
2024-06-25 15:58:44 +0530

    dasd

--- Nodejs/nodejs_fakeapi/api/index.js
+++ Nodejs/nodejs_fakeapi/api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-const setupSwagger = require("../swagger");
+const setupSwagger = require("./docs/swagger");
 setupSwagger(app);
 
 app.use(express.json());
