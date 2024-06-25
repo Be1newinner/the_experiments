233bc0375fc92c039ff9a209f3018ba0846d787b
Vijay Kumar <Be1newinner@gmail.com>
2024-06-25 16:52:28 +0530

    allowed origin *

--- Nodejs/nodejs_fakeapi/api/index.js
+++ Nodejs/nodejs_fakeapi/api/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-
+const cors = require("cors");
 // const setupSwagger = require("./docs/swagger");
 // setupSwagger(app);
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const products = Array.from({ length: 30 }, (v, k) => ({
   id: k + 1,
